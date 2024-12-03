1a26c64695d3999173257621d4e1e178f0e33529
KotouraSan <111898848+KotouraSan@users.noreply.github.com>
2024-12-03 15:13:20 +0500

    java project

---
+++ Auth_service/src/test/java/uz/ksan/backend/Auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ksan.backend.Auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
