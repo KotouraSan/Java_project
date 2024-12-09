43bc3d1b3ec4eaed2807d2c2eb277b7e6735c35f
magnasoldier <magnasoldier@gmail.com>
2024-12-09 19:23:15 +0000

    many fixes in docker (to be continued)

---
+++ init-scripts/init-databases.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE project_auth;
+CREATE DATABASE project_customer;
+CREATE DATABASE project_delivery;
+CREATE DATABASE project_order;
+CREATE DATABASE project_shop;
+
+GRANT ALL PRIVILEGES ON DATABASE project_auth TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE project_customer TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE project_delivery TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE project_order TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE project_shop TO postgres;
