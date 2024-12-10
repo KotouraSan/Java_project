114ca32f0bc58538124407aefb37ec64b8a0364f
Марат Киньзябулатов <maratkinzabulatov@MacBook-Air-Marat.local>
2024-12-10 10:28:50 +0500

    many updates for zipkin and docker

--- init-scripts/init-databases.sql
+++ init-scripts/init-databases.sql
@@ -3,9 +3,11 @@ CREATE DATABASE project_customer;
 CREATE DATABASE project_delivery;
 CREATE DATABASE project_order;
 CREATE DATABASE project_shop;
+CREATE DATABASE zipkin;
 
 GRANT ALL PRIVILEGES ON DATABASE project_auth TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE project_customer TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE project_delivery TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE project_order TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE project_shop TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE zipkin TO postgres;
